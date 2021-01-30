f57ffd6f6a6e65f291e898337bfe94742376435b
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 00:48:32 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -52,7 +52,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     let before = conn.menu.before || `${conn.getName(conn.user.jid)} • Bot\n\nHai, %name!\n\nTotal: *%exp XP*\nLimit Anda: *%limit Limit*\nTanggal: *%week, %date*\n Waktu: *%time*\n_Uptime: %uptime_\n  %totalreg User in database\n\n \n\n DuelStandby \n\n%readmore`
     let header = conn.menu.header || '╭════•›「 %category 」'
     let body   = conn.menu.body   || '┠❥ %cmd%islimit'
-    let footer = conn.menu.footer || '╰═══════════════\n'
+    let footer = conn.menu.footer || '╰═══════════════'
     let after  = conn.menu.after  || conn.user.jid == global.conn.user.jid ? '' : `\nPowered by: ${global.conn.user.jid.split`@`[0]}`
     let _text  = before + '\n'
     for (let tag in groups) {
