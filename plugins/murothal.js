60ad671ebb2647940e67e70a2a9906929e37e4e8
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 00:39:49 +0700

    Update murothal.js

--- plugins/murothal.js
+++ plugins/murothal.js
@@ -39,7 +39,7 @@ Juz 30 ⇨ http://j.mp/2bFREcc
 `.trim(), m)
 }
 handler.help = ['murothal']
-handler.tags = ['quotes']
+handler.tags = ['']
 handler.command = /^(murothal)$/i
 handler.owner = false
 handler.mods = false
