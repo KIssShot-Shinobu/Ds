e2efed5e4c754c462827f2a521000294752c8953
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 01:02:50 +0700

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -15,7 +15,7 @@ let handler  = async (m, { conn, text }) => {
   }
 }
 handler.help = ['ttp <teks>']
-handler.tags = ['sticker']
+handler.tags = ['']
 handler.command = /^ttp$/i
 handler.owner = false
 handler.mods = false
