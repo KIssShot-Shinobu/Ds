8f7d6bc899672cd4be07778d6a609683e4b68f4b
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 11:39:50 +0700

    Update kapankah2.js

--- plugins/kapankah2.js
+++ plugins/kapankah2.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, command, text }) => {
 `.trim(), m)
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <pertanyaan>')
-handler.tags = ['fun']
+handler.tags = ['']
 handler.command = /^kapan(kah)?/i
 handler.owner = false
 handler.mods = false
