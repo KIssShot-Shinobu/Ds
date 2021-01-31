8dd30e13a49e05e2deeedc5f771c0dba73a3298d
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 12:10:14 +0700

    Update event.js

--- plugins/event.js
+++ plugins/event.js
@@ -2,7 +2,13 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
 event yang sedang berlangsung di duel stanby! :
 
-1. Liga internal season 2 
+1. Liga internal season 2 :
+  Minggu, 31 Januari 2021 
+  20:20 WIB 
+  7 Complicated vs Domino
+
+  Phobos vs Shadow Slaye
+
 
 2. KOG rush : Senin 01 Feburari 2021 
     Max 6 jam   08:00 - 14:00 WIB 
