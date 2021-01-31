86c98b99e61f9e499ee5d76f74dff799688393b0
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-01-31 10:30:17 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -46,10 +46,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
           if (menu.help) groups[tag].push(menu)
     }
     conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || `${conn.getName(conn.user.jid)} • Bot\n\nHai, %name!\n\nTotal: *%exp XP*\nLimit Anda: *%limit Limit*\nTanggal: *%week, %date*\n Waktu: *%time*\n_Uptime: %uptime_\n  %totalreg User in database\n Gunakan ".menu" untuk membuka menu ini.
-
-
-*DUEL STANDBY BOT MENU versi 1.0 by KissShot* \n\n%readmore`
+    let before = conn.menu.before || `${conn.getName(conn.user.jid)} •*DUEL STANDBY BOT MENU versi 1.0*\n\nHai, %name!\n\nTotal: *%exp XP*\nLimit Anda: *%limit Limit*\nTanggal: *%week, %date*\n Waktu: *%time*\n_Uptime: %uptime_\n  %totalreg User in database\n Gunakan ".menu" untuk membuka menu ini.\n%readmore`
     let header = conn.menu.header || '╭════•›「 %category 」'
     let body   = conn.menu.body   || '┠❥ %cmd%islimit'
     let footer = conn.menu.footer || '╰═══════════════\n'
